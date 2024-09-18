12e3c5747a7f62d1b9c6fb50a729dc3a6bf9b029
hamed <hamed2005@gmail.com>
2024-09-18 21:31:39 +0200

    Create a consolidated list of languages to be used by the app and frontend

---
+++ lingualink/supported_languages.py
@@ -0,0 +1,9 @@
+# supported_languages.py
+
+# Dictionary that defines languages and which method supports them
+supported_languages = {
+    'de': {'name': 'German', 'wikipedia': True, 'machine': True},
+    'fr': {'name': 'French', 'wikipedia': True, 'machine': True},
+    'nl': {'name': 'Dutch', 'wikipedia': True, 'machine': False},
+    'fa': {'name': 'Farsi', 'wikipedia': True, 'machine': False},
+}

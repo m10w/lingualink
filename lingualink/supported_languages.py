5c97a946ce002904e36e66f14e55419e494f785a
hamed <hamed2005@gmail.com>
2024-09-19 21:47:32 +0200

    add machine translation model name to the unified list.
    Enable machine translation for Dutch

--- lingualink/supported_languages.py
+++ lingualink/supported_languages.py
@@ -2,9 +2,32 @@
 
 # Dictionary that defines languages and which method supports them
 supported_languages = {
-    'de': {'name': 'German', 'wikipedia': True, 'machine': True},
-    'fr': {'name': 'French', 'wikipedia': True, 'machine': True},
-    'nl': {'name': 'Dutch', 'wikipedia': True, 'machine': False},
-    'fa': {'name': 'Farsi', 'wikipedia': True, 'machine': False},
-    'es': {'name': 'Spanish', 'wikipedia': True, 'machine': False}
+    'de': {
+        'name': 'German',
+        'wikipedia': True,
+        'machine': True,
+        'model_name': 'Helsinki-NLP/opus-mt-en-de'
+    },
+    'fr': {
+        'name': 'French',
+        'wikipedia': True,
+        'machine': True,
+        'model_name': 'Helsinki-NLP/opus-mt-en-fr'
+    },
+    'nl': {
+        'name': 'Dutch',
+        'wikipedia': True,
+        'machine': True,
+        'model_name': 'Helsinki-NLP/opus-mt-en-nl'
+        },
+    'fa': {
+        'name': 'Farsi',
+        'wikipedia': True,
+        'machine': False,
+        'model_name': None},
+    'es': {
+        'name': 'Spanish',
+        'wikipedia': True,
+        'machine': False,
+        'model_name': None}
 }

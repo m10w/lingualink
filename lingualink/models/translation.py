17687193321b6a6bfafd51bfe322339a3a68c38c
hamed <hamed2005@gmail.com>
2024-09-19 21:59:21 +0200

    use the unified language list to get language code and model name; preload models for efficiency;

--- lingualink/models/translation.py
+++ lingualink/models/translation.py
@@ -1,20 +1,28 @@
+from ..supported_languages import supported_languages
 from transformers import MarianMTModel, MarianTokenizer
 from functools import lru_cache
+import logging
 
-# Dictionary to store the model name per language code
-model_name_dict = {
-    'de': 'Helsinki-NLP/opus-mt-en-de',  # English to German
-    'fr': 'Helsinki-NLP/opus-mt-en-fr',  # English to French
-    'es': 'Helsinki-NLP/opus-mt-en-es'   # English to Spanish
-}
+logging.basicConfig(level=logging.INFO)
+
+model_cache = {} # Cache for storing models
+def preload_models():
+    # Preload all models into cache at startup
+    for language_code, lang_data in supported_languages.items():
+        model_name = lang_data.get('model_name')
+        if model_name:
+            model = MarianMTModel.from_pretrained(model_name)
+            tokenizer = MarianTokenizer.from_pretrained(model_name)
+            model_cache[language_code] = (model, tokenizer)
+            logging.info(f"Preloaded model for language: {language_code}.")
+
+# Automatically preload all models.
+preload_models()
 
 # Function to load model and tokenizer
 def load_model(language_code):
-    model_name = model_name_dict.get(language_code)
-    if model_name:
-        model = MarianMTModel.from_pretrained(model_name)
-        tokenizer = MarianTokenizer.from_pretrained(model_name)
-        return model, tokenizer
+    if language_code in model_cache:
+        return model_cache[language_code]
     else:
         raise ValueError(f"No model available for language code {language_code}")
 

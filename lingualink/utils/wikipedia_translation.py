eafbf6cbb4c5472a505a0082db1147a1e09c4f21
hamed <hamed2005@gmail.com>
2024-09-25 21:54:30 +0200

    Add fuzzy matching functionality to wikipedia translation

--- lingualink/utils/wikipedia_translation.py
+++ lingualink/utils/wikipedia_translation.py
@@ -1,4 +1,5 @@
 import requests
+from fuzzywuzzy import fuzz, process
 
 # <TODO>: this should be modified later to be able to select the input language
 WIKIPEDIA_API_URL = "https://en.wikipedia.org/w/api.php"
@@ -16,11 +17,36 @@ def get_wikipedia_translation(word, target_language):
     data = response.json()
 
     pages = data.get("query", {}).get("pages", {})
+    langlinks = None
     for page_id, page_data in pages.items():
         langlinks = page_data.get("langlinks", [])
-        if langlinks:
-            return langlinks[0][
-                "*"
-            ]  # Return the translated title in the target language
 
-    return None  # If no translation is found
+    # If no exact match found, use fuzzy matching
+    if not langlinks:
+        return fuzzy_match_titles(word, target_language)
+
+    return langlinks[0]['*'] if langlinks else None
+
+def fuzzy_match_titles(word, target_language):
+    # Query wikipedia for possible matches (e.g., list all pages in the target language
+    search_params = {
+        'action': 'query',
+        'list': 'search',
+        'srsearch': word,
+        'format': 'json',
+    }
+    search_response = requests.get(WIKIPEDIA_API_URL, params=search_params)
+    search_data = search_response.json()
+
+    # Get titles from seach results
+    titles =[result['title'] for result in search_data.get('query', {}).get('search', [])]
+
+    if titles:
+        # Use fuzzy matching to find the best match
+        best_match, score = process.extractOne(word, titles, scorer=fuzz.ratio)
+
+        # If the score is good enough, return the best match
+        if score > 50:
+            return f"Did you mean: {best_match}?"
+
+    return None

2aaa8cbcc47453731550efce9455bd75d4c0638d
hamed <hamed2005@gmail.com>
2024-09-25 21:53:21 +0200

    Use absolute path for imports

--- lingualink/app.py
+++ lingualink/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from lingualink.models.translation import translate_text
-from .utils.wikipedia_translation import get_wikipedia_translation
+from lingualink.utils.wikipedia_translation import get_wikipedia_translation
 import logging
-from .supported_languages import supported_languages
+from lingualink.supported_languages import supported_languages
 
 logging.basicConfig(level=logging.INFO)
 

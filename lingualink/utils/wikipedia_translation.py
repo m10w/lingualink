955b08e9b7b4bd7d4fd5c3b596294519e06168ea
hamed <hamed2005@gmail.com>
2024-09-28 21:42:41 +0200

    Improve fuzzy matching by checking if a translation in target language exists.

--- lingualink/utils/wikipedia_translation.py
+++ lingualink/utils/wikipedia_translation.py
@@ -4,8 +4,30 @@ from fuzzywuzzy import fuzz, process
 # <TODO>: this should be modified later to be able to select the input language
 WIKIPEDIA_API_URL = "https://en.wikipedia.org/w/api.php"
 
-
 def get_wikipedia_translation(word, target_language):
+    """
+    Orchestrates the process of finding the best Wikipedia translation by first
+    attempting an exact match, and if necessary, falling back to fuzzy matching.
+    """
+    # Attempt to get an exact match
+    exact_translation = exact_match_titles(word, target_language)
+
+    if exact_translation:
+        return {"suggestion": None, "translation": exact_translation}
+
+    # If no exact match is found, attempt fuzzy matching
+    fuzzy_result = fuzzy_match_titles(word, target_language)
+
+    if fuzzy_result["translation"]:
+        return {"suggestion": fuzzy_result["suggestion"], "translation": fuzzy_result["translation"]}
+
+    return {"suggestion": None, "translation": "No Wikipedia translation available"}
+
+
+def exact_match_titles(word, target_language):
+    """
+    Attempts to find an exact Wikipedia translation for the given word in the target language.
+    """
     params = {
         "action": "query",
         "titles": word,
@@ -20,15 +42,15 @@ def get_wikipedia_translation(word, target_language):
     langlinks = None
     for page_id, page_data in pages.items():
         langlinks = page_data.get("langlinks", [])
-
-    # If no exact match found, use fuzzy matching
-    if not langlinks:
-        return fuzzy_match_titles(word, target_language)
-
-    return langlinks[0]['*'] if langlinks else None
+        if langlinks:
+            return langlinks[0]['*'] # Return the translated title in the target language
+    return None
 
 def fuzzy_match_titles(word, target_language):
-    # Query wikipedia for possible matches (e.g., list all pages in the target language
+    """
+    Attempts to find a partial match for the word using fuzzy matching and checks
+    if the best match has a valid translation in the target language.
+    """
     search_params = {
         'action': 'query',
         'list': 'search',
@@ -45,8 +67,9 @@ def fuzzy_match_titles(word, target_language):
         # Use fuzzy matching to find the best match
         best_match, score = process.extractOne(word, titles, scorer=fuzz.ratio)
 
-        # If the score is good enough, return the best match
-        if score > 50:
-            return f"Did you mean: {best_match}?"
+        # Validate that the best match has a translation in the target language
+        translation = exact_match_titles(best_match, target_language)
+        if translation and score > 0:
+            return {"suggestion": f'{best_match} {score}', "translation": translation}
 
-    return None
+    return {"suggestion": None, "translation": None}
